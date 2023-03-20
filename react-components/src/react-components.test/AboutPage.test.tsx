16c6e451382eb7ffddd547f52d634a8733195a8e
Alina <almaksimovic01@gmail.com>
2023-03-20 17:18:33 +0300

    fix: fixed linter warning

--- react-components/src/react-components.test/AboutPage.test.tsx
+++ react-components/src/react-components.test/AboutPage.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 
 import AboutPage from '../pages/AboutPage'
