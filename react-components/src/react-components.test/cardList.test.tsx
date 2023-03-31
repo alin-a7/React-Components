08c9c82491e7b0c0fb455a6304e26bc4a9d2ab18
Alina <almaksimovic01@gmail.com>
2023-03-31 21:41:16 +0300

    feat: add test for CardsList on the FormPage

--- react-components/src/react-components.test/cardList.test.tsx
+++ react-components/src/react-components.test/cardList.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
 
+import CardsList from '../pages/FormsPage/components/CardsList'
 import Card from '../pages/CardsPage/components/Card'
 
 import { allProducts } from '../constants/allProducts'
 
 describe('CardList', () => {
-  it('render list', () => {
+  it('render list on the CardsPage', () => {
     class CardList extends React.Component {
       render() {
         return allProducts.map((item) => <Card key={item.id} {...item} />)
@@ -14,5 +15,27 @@ describe('CardList', () => {
     }
     render(<CardList />)
     expect(screen.getAllByTestId('card').length).toEqual(20)
+  })
+  it('render list on the FormPage', () => {
+    const cardArray = [
+      {
+        text: 'text',
+        date: 'date',
+        language: 'ru',
+        gender: 'male',
+        file: 'img',
+        agreement: true,
+      },
+      {
+        text: 'text',
+        date: 'date',
+        language: 'ru',
+        gender: 'male',
+        file: 'img',
+        agreement: true,
+      },
+    ]
+    render(<CardsList cardArray={cardArray} />)
+    expect(screen.getAllByTestId('form-card').length).toEqual(2)
   })
 })
