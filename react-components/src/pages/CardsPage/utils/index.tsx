c425c86858670183136f30292a395d1886069ca5
Alina <almaksimovic01@gmail.com>
2023-04-05 18:52:01 +0300

    feat: add test for ModalCard and CardsPage

--- react-components/src/pages/CardsPage/utils/index.tsx
+++ react-components/src/pages/CardsPage/utils/index.tsx
@@ -1,6 +1,6 @@
 import { SyntheticEvent } from "react"
 
-const BASE_URL = 'https://rickandmortyapi.com/api/character'
+export const BASE_URL = 'https://rickandmortyapi.com/api/character'
 
 export const getURL = (name: string) => {
   return name ? `${BASE_URL}/?name=${name}` : BASE_URL
--- react-components/src/tests/setup.ts
+++ react-components/src/tests/setup.ts
@@ -1,1 +1,13 @@
+/* eslint-env jest */
 import '@testing-library/jest-dom'
+import { server } from '../mooks/server'
+
+beforeAll(() =>
+  server.listen({
+    onUnhandledRequest: 'error',
+  }),
+)
+
+afterEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
